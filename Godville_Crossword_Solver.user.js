79927c3021b920ede11f4e144ee7d012bf495b9b
popcornrulez <popcornrulezz@gmail.com>
2017-12-06 19:25:37 -0800

    Add files via upload

---
+++ Godville_Crossword_Solver.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name          Godville Crossword Solver
+// @description   Solves the Daily Crossword Puzzle in the Godville Times webpage
+// @include       /^https?://godvillegame\.com/news/
+// @version       1.0.0
+// ==/UserScript==
